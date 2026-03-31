af3c760f521b99c1a9f1a9e195c032e9ae7da4a4
Evelrine <dispater.nox@gmail.com>
2026-03-31 23:11:59 +0300

    Написал функцию сортировки

--- lesson 1.3/main.go
+++ lesson 1.3/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sort"
+)
 
+func sortNumbers(input []int) (output []int) {
+	var sortNumbers []int = input
+	sort.Slice(sortNumbers, func(i, j int) bool {
+		return sortNumbers[i] > sortNumbers[j] // от большего к меньшему
+	})
+	output = sortNumbers // присваиваем результат в output
+	fmt.Println(output)
+	return
+}
+
+func main() {
+	sortNumbers([]int{5, 2, 4, 3, 1}) // передаём срез чисел
 }
