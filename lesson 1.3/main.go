3e608382ef5a3d78028c3585e72f4f180f5a4728
Evelrine <dispater.nox@gmail.com>
2026-04-02 19:56:53 +0300

    Убрал лишнюю переменную, для вывода среднего использую теперь float

--- lesson 1.3/main.go
+++ lesson 1.3/main.go
@@ -5,19 +5,20 @@ import (
 	"sort"
 )
 
-func sortNumbers(input []int) (output []int) {
-	var sortNumbers []int = input
-	sort.Slice(sortNumbers, func(i, j int) bool {
-		return sortNumbers[i] > sortNumbers[j]
+func sortNumbers(input []int) {
+	sort.Slice(input, func(i, j int) bool {
+		return input[i] > input[j]
 	})
-	output = sortNumbers
-	fmt.Println("Отсортированные элементы:", output)
-	fmt.Println("Самое большое число:", output[0])
-	fmt.Println("Самое маленькое число:", output[4])
-	fmt.Println("Среднее-арифметическое:", (output[0]+output[1]+output[2]+output[3]+output[4])/5)
-	return
+
+	fmt.Println("Отсортированные элементы:", input[0], input[1], input[2], input[3], input[4])
+	fmt.Println("Самое большое число:", input[0])
+	fmt.Println("Самое маленькое число:", input[4])
+
+	sum := input[0] + input[1] + input[2] + input[3] + input[4]
+	average := float64(sum) / 5.0
+	fmt.Printf("Среднее-арифметическое: %.2f\n", average)
 }
 
 func main() {
-	sortNumbers([]int{500, 2, 40, 39, -1})
+	sortNumbers([]int{50, 2, 41, 3, 10})
 }
