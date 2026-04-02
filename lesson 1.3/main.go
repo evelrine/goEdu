75f162effe84bf333772840c3feb24b36bf74bf8
Evelrine <dispater.nox@gmail.com>
2026-04-02 20:55:52 +0300

    Добавил скан для ввода чисел, перенес принт из метода сортировки в мейн

--- lesson 1.3/main.go
+++ lesson 1.3/main.go
@@ -5,20 +5,30 @@ import (
 	"sort"
 )
 
-func sortNumbers(input []int) {
+func sortNumbers(input []int) []int {
 	sort.Slice(input, func(i, j int) bool {
 		return input[i] > input[j]
 	})
-
-	fmt.Println("Отсортированные элементы:", input[0], input[1], input[2], input[3], input[4])
-	fmt.Println("Самое большое число:", input[0])
-	fmt.Println("Самое маленькое число:", input[4])
-
-	sum := input[0] + input[1] + input[2] + input[3] + input[4]
-	average := float64(sum) / 5.0
-	fmt.Printf("Среднее-арифметическое: %.2f\n", average)
+	return input
 }
 
 func main() {
-	sortNumbers([]int{50, 2, 41, 3, 10})
+	numbers := make([]int, 5)
+
+	fmt.Println("Введите 5 натуральных чисел:")
+
+	for i := 0; i < 5; i++ {
+		fmt.Printf("Число %d: ", i+1)
+		fmt.Scan(&numbers[i])
+	}
+
+	sortedNumbers := sortNumbers(numbers)
+
+	fmt.Println("Отсортированные элементы:", sortedNumbers[0], sortedNumbers[1], sortedNumbers[2], sortedNumbers[3], sortedNumbers[4])
+	fmt.Println("Самое большое число:", sortedNumbers[0])
+	fmt.Println("Самое маленькое число:", sortedNumbers[4])
+
+	sum := sortedNumbers[0] + sortedNumbers[1] + sortedNumbers[2] + sortedNumbers[3] + sortedNumbers[4]
+	average := float64(sum) / 5.0
+	fmt.Printf("Среднее-арифметическое: %.2f\n", average)
 }
